cb03a0aef34d7d388a9b88bff8094157a35d21bf
Hazimeteotukaisab <168907207+Hazimeteotukaisab@users.noreply.github.com>
2024-09-16 19:57:06 +0900

    Update main.lua

--- Main/main.lua
+++ Main/main.lua
@@ -1,4 +1,4 @@
 local api = gg.makeRequest("https://raw.githubusercontent.com/Hazimeteotukaisab/OtukaiScriptGG-45451919/main/maintenancecheck.lua");
 if not pcall(load(api.content)) then
-gg.toast("ようこそ")
+gg.alert("ようこそ")
 end
