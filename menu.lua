e24947ac1a9ffbc2c0d1c076922e55a15793a713
Hazimeteotukaisab <168907207+Hazimeteotukaisab@users.noreply.github.com>
2024-09-20 11:47:53 +0900

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,47 +1,21 @@
 Ax = gg.choice({
-"高速移動オン",
-"高速移動オフ",
-"戻る"}, nil, "高速移動")
+"メニュー",
+"メニュー",
+"戻る"}, nil, "メニュー")
 
 if Ax ~= nil then
 
 if Ax == 1 then
-gg.clearResults()
-gg.setRanges(gg.REGION_ANONYMOUS)
-Ax = gg.prompt({"足の速さの編集値を入力"},{5},{"number"})
-if not Ax then return end
-
-gg.clearResults()
-gg.setRanges(gg.REGION_ANONYMOUS)
-gg.searchNumber("1",gg.TYPE_DOUBLE)
-gg.getResults(9999)
-gg.editAll(Ax[1],gg.TYPE_DOUBLE)
-gg.clearResults()
-gg.toast("✅ON✅")
-end
+  end
 
 if Ax == 2 then
-gg.clearResults()
-gg.clearResults()
-gg.setRanges(gg.REGION_ANONYMOUS)
-Ax = gg.prompt({"足の速さの編集値を入力"},{5},{"number"})
-if not Ax then return end
-
-gg.clearResults()
-gg.setRanges(gg.REGION_ANONYMOUS)
-gg.searchNumber(Ax[1],gg.TYPE_DOUBLE)
-gg.getResults(9999)
-gg.editAll("1",gg.TYPE_DOUBLE)
-gg.clearResults()
-gg.toast("✖OFF✖")
-end
+  end
 
 if Ax == 3 then
 local api = gg.makeRequest("");
 if not pcall(load(api.content)) then
-end
-end
-
+    end
+  end
 if Lext == 1 then
 Main()
 end
