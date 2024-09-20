7e450e44727ed0f082e8ea0f4b3d7f52704a9637
Hazimeteotukaisab <168907207+Hazimeteotukaisab@users.noreply.github.com>
2024-09-20 21:42:02 +0900

    Update All.lua

--- Menu/All.lua
+++ Menu/All.lua
@@ -13,7 +13,7 @@ Ax = gg.choice({
 if Ax ~= nil then
 
 if Ax == 1 then
-local api = gg.makeRequest("");
+local api = gg.makeRequest("https://raw.githubusercontent.com/Hazimeteotukaisab/OtukaiScriptGG-45451919/main/Menu/Menu1/Menu.lua");
 if not pcall(load(api.content)) then
 gg.alert("ようこそ")
 end
